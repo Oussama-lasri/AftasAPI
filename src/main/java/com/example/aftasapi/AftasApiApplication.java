d88aa86ad9d48c3a43aeab5afa3f3775dc5d695a
Oussama-lasri <oussama.lasri.2k@gmail.com>
2023-12-09 17:51:34 +0100

    Initial Project

---
+++ src/main/java/com/example/aftasapi/AftasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.aftasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftasApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/aftasapi/Enums/IdentityDocumentType.java
@@ -0,0 +1,2 @@
+package com.example.aftasapi.Enums;public enum IdentityDocumentType {
+}
